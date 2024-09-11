9b91ce290724b24c1043f0159100349bbe779165
muskan <muskanloach984@gmail.com>
2024-09-11 23:50:12 +0530

    skills page

---
+++ src/components/Skills.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div name="skills" className='bg-[#0a192f] '>
+        <div>
+            <div>
+                <p>Experience</p>
+                <p>//These are the Technologies i have worked with.</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/html.png"></img>
+                <p>HTML</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/css.png"></img>
+                <p>CSS</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/react.svg"></img>
+                <p>React.js</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/js.png"></img>
+                <p>JavaScript</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/tailwind.png"></img>
+                <p>Tailwind CSS</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/sql.png"></img>
+                <p>SQL</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/github.png"></img>
+                <p>GitHub</p>
+            </div>
+            <div>
+                <img className='w-20 mx-auto' src="src/assets/ts.png"></img>
+                <p>TypeScript</p>
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Skills
