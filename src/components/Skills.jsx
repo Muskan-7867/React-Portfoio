392a05f290332868c1e062218fd29b230e88184c
muskan <muskanloach984@gmail.com>
2024-09-12 16:37:06 +0530

    skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,44 +2,46 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div name="skills" className='bg-[#0a192f] '>
-        <div>
-            <div>
-                <p>Experience</p>
-                <p>//These are the Technologies i have worked with.</p>
+    <div name="skills" className=' w-full h-screen bg-[#0a192f]  text-gray-300 '>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div >
+                <p className='text-4xl font-bold inline border-b-4 border-emerald-600-600'>Skills</p>
+                <p className='py-4'>//These are the Technologies i have worked with.</p>
             </div>
-            <div>
+            <div className='grid  w-full grid-cols-2 sm:grid-cols-4 gap-10 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src="src/assets/html.png"></img>
-                <p>HTML</p>
+                <p className='my-4'>HTML</p>
             </div>
-            <div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src="src/assets/css.png"></img>
-                <p>CSS</p>
+                <p className='my-4'>CSS</p>
             </div>
-            <div>
-                <img className='w-20 mx-auto' src="src/assets/react.svg"></img>
-                <p>React.js</p>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-16 mx-auto' src="src/assets/js.png"></img>
+                <p className='my-4'>JavaScript</p>
             </div>
-            <div>
-                <img className='w-20 mx-auto' src="src/assets/js.png"></img>
-                <p>JavaScript</p>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-16 mx-auto' src="src/assets/react.svg"></img>
+                <p className='my-4'>React.js</p>
             </div>
-            <div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-16 mx-auto' src="src/assets/ts.png"></img>
+                <p className='my-4'>TypeScript</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src="src/assets/mongodb.png"></img>
+                <p className='my-4'>MongoDB</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-16 mx-auto' src="src/assets/sql.png"></img>
+                <p className='my-4'>SQL</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src="src/assets/tailwind.png"></img>
-                <p>Tailwind CSS</p>
+                <p className='my-4'>Taiwind CSS</p>
             </div>
-            <div>
-                <img className='w-20 mx-auto' src="src/assets/sql.png"></img>
-                <p>SQL</p>
-            </div>
-            <div>
-                <img className='w-20 mx-auto' src="src/assets/github.png"></img>
-                <p>GitHub</p>
-            </div>
-            <div>
-                <img className='w-20 mx-auto' src="src/assets/ts.png"></img>
-                <p>TypeScript</p>
-
+           
             </div>
         </div>
       
