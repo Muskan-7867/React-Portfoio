069bf6364584601b1c418a9899699c18062b562e
muskan <muskanloach984@gmail.com>
2024-09-15 17:04:10 +0530

    main fie

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
 import Work from './components/Work'
+import Contact from './components/Contact'
+
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <About />
       <Skills />
       <Work />
+      <Contact/>
     </div>
   )
 }
