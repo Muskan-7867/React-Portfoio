e6c4388664fc8ce908776b50687ac61b4675789b
muskan <muskanloach984@gmail.com>
2024-09-16 17:14:19 +0530

    contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,14 @@ const Contact = () => {
   return (
     <div name="contact" className="w-full h-screen bg-[#0a192f] flex justify-center  text-gray-300 py-16 items-center p-4">
     <form method='POST' action="https://getform.io/f/bdrywqeb" className=" flex flex-col   w-full max-w-[600px]">
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-emerald-600">Contact</p>
+      <div className="pb-8 animate-slideIn">
+        <p className="text-4xl font-bold inline border-b-4 border-emerald-600 ">Contact</p>
     
       </div>
-      <input className='p-2 bg-[#ccd6f6] text-gray-900  rounded-lg mb-4' type='text' placeholder='Name' name='name'></input>
-      <input className='p-2 bg-[#ccd6f6]  text-gray-900 rounded-lg mb-4' type='email' placeholder='Email' name='email'></input>
-      <textarea className='p-2 bg-[#ccd6f6] text-gray-900 rounded-lg ' rows="10" type='message' placeholder='Message' name='message'></textarea>
-      <button className='text-white border-2 border-emerald-600 hover:bg-emerald-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+      <input className='p-2 bg-[#ccd6f6] animate-slideInFromRight text-gray-900  rounded-lg mb-4' type='text' placeholder='Name' name='name'></input>
+      <input className='p-2 bg-[#ccd6f6] animate-slideIn text-gray-900 rounded-lg mb-4' type='email' placeholder='Email' name='email'></input>
+      <textarea className='p-2 bg-[#ccd6f6] animate-slideInFromRight text-gray-900 rounded-lg ' rows="10" type='message' placeholder='Message' name='message'></textarea>
+      <button className='text-white border-2 animate-slideIn border-emerald-600 hover:bg-emerald-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
     </form>
     </div>
    
