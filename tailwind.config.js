c40a27180607991f85f6838648eaf109f0439fdf
muskan <muskanloach984@gmail.com>
2024-09-16 17:16:13 +0530

    tailwind css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,42 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out infinite',
+        typing: 'typing 3.5s steps(40, end) infinite, blink .75s step-end infinite',
+        slideIn: 'slideIn 5s ease-in-out infinite', // Updated animation for sliding in
+        slideInFromRight: 'slideInFromRight 5s ease-in-out infinite', // Existing animation
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+        typing: {
+          '0%': { width: '0%' },
+          '50%': { width: '100%' },
+          '100%': { width: '0%' },
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' },
+          '100%': { borderColor: 'white' },
+        },
+        slideIn: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '10%': { transform: 'translateX(0)', opacity: '1' }, // Slide in
+          '90%': { transform: 'translateX(0)', opacity: '1' }, // Stay in view
+          '100%': { transform: 'translateX(-100%)', opacity: '0' }, // Slide out
+        },
+        slideInFromRight: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '10%': { transform: 'translateX(0)', opacity: '1' }, // Slide in
+          '90%': { transform: 'translateX(0)', opacity: '1' }, // Stay in view
+          '100%': { transform: 'translateX(100%)', opacity: '0' }, // Slide out
+        },
+      },
+    },
   },
   plugins: [],
 }
