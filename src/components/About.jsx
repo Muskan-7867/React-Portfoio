ba63da12b528a84f86835afd214dad3f6edd80c6
muskan <muskanloach984@gmail.com>
2024-09-16 15:23:17 +0530

    text animation add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,11 +13,11 @@ const About = () => {
           <div></div>
           </div>
           <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 ">
-            <div className="sm:text-right text-4xl font-bold">
+            <div className="sm:text-right text-4xl font-bold animate-typing">
               <p>Hi I'm Muskan, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-                <p>I am a passionate Frontend Developer specializing in building dynamic, responsive, and user-friendly websites and web applications. With a strong foundation in HTML, CSS, and JavaScript, along with experience in frameworks like React, I bring designs to life with clean, efficient code.
+                <p className="animate-fadeIn">I am a passionate Frontend Developer specializing in building dynamic, responsive, and user-friendly websites and web applications. With a strong foundation in HTML, CSS, and JavaScript, along with experience in frameworks like React, I bring designs to life with clean, efficient code.
                      I thrive on solving complex problems, optimizing performance, and creating seamless user experiences.</p>
             </div>
           </div>
