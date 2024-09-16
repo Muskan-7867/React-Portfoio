b658b458277b8c62677b6f1440e3f018f6c66880
muskan <muskanloach984@gmail.com>
2024-09-16 17:15:11 +0530

    skill page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,11 +5,11 @@ const Skills = () => {
     <div name="skills" className=' w-full h-screen bg-[#0a192f]  text-gray-300 '>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div >
-                <p className='text-4xl font-bold inline border-b-4 border-emerald-600-600'>Skills</p>
-                <p className='py-4'>These are the Technologies i have worked with.</p>
+                <p className='text-4xl font-bold inline border-b-4 border-emerald-600 animate-slideIn'>Skills</p>
+                <p className='py-6 animate-slideInFromRight'>These are the Technologies i have worked with.</p>
             </div>
-            <div className='grid  w-full grid-cols-2 sm:grid-cols-4 gap-10 text-center py-8'>
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='grid  w-full grid-cols-2 sm:grid-cols-4 animate-slideIn gap-10 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110  duration-500'>
                 <img className='w-20 mx-auto' src="/images/html.png"></img>
                 <p className='my-4'>HTML</p>
             </div>
