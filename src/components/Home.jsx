00350e78ce311cc71d2371198dd41609c5b1d5ef
muskan <muskanloach984@gmail.com>
2024-09-16 17:14:39 +0530

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,26 +3,35 @@ import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f] ">
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-emerald-500">Hi, my Name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Muskan</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I am a Frontend Developer</h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
+        {/* Applying slideIn animation */}
+        <p className="text-emerald-500 animate-slideIn">Hi, my Name is</p>
+        
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] animate-slideInFromRight">
+          Muskan
+        </h1>
+        
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0] animate-slideIn">
+          I am a Frontend Developer
+        </h2>
+        
+        <p className="text-[#8892b0] py-4 max-w-[700px] animate-slideInFromRight">
           A Frontend Developer builds and designs user interfaces for websites
           and web apps using HTML, CSS, JavaScript, and frameworks like React.
           They ensure a responsive, visually appealing, and interactive user
           experience while collaborating with designers and backend developers.
         </p>
         
-        <div >
+        <div>
           <button
-            className="text-white group border-2 px-6 py-3 my-2 flex items-center  hover:border-emerald-500 hover:bg-emerald-500"
+            className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:border-emerald-500 hover:bg-emerald-500"
             type="submit"
           >
             View Work
             <span className="group-hover:rotate-90 duration-300">
-            <FaArrowRight  className="ml-3"/></span> 
+              <FaArrowRight className="ml-3" />
+            </span>
           </button>
         </div>
       </div>
