3b2aaf89f0994b147c8fa6888b536a5341d300df
muskan <muskanloach984@gmail.com>
2024-09-20 22:28:56 +0530

    contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <p className="text-4xl font-bold inline border-b-4 border-emerald-600 ">Contact</p>
     
       </div>
-      <input className='p-2 bg-[#ccd6f6] animate-slideInFromRight text-gray-900  rounded-lg mb-4' type='text' placeholder='Name' name='name'></input>
+      <input className='p-2 bg-[#ccd6f6]   animate-slideInFromRight text-gray-900  rounded-lg mb-4' type='text' placeholder='Name' name='name'></input>
       <input className='p-2 bg-[#ccd6f6] animate-slideIn text-gray-900 rounded-lg mb-4' type='email' placeholder='Email' name='email'></input>
       <textarea className='p-2 bg-[#ccd6f6] animate-slideInFromRight text-gray-900 rounded-lg ' rows="10" type='message' placeholder='Message' name='message'></textarea>
       <button className='text-white border-2 animate-slideIn border-emerald-600 hover:bg-emerald-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
